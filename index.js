dba1a48156552d68d153aa15420ebb8b9db530e3
Andres Gutierrez <andres_mau.gutierrez@uao.edu.co>
2022-09-16 17:19:04 -0500

    Custom types y documentación de querys

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 'use strict'
 
 // Se importan los modulos de graphql
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql')
 
-const express = require('express');
+const express = require('express')
 
-const { graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql')
 
-const app = express();
-const port = process.env.port || 3000;
+const { readFileSync } = require('fs');
 
-// Se define el esquema
-const schema = buildSchema(`
-  type Query {
-    hello: String,
-  }
-`);
+const { join } = require('path')
 
-// Configurar resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hola mundo'
-    },
-}
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+// Se lee el esquema
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'),'utf-8'))
 
 app.use('/api', graphqlHTTP({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
-app.listen( port, () => {
-    console.log('Server escuchando en el puerto http://localhost:' + port + '/api');
+app.listen(port, () => {
+  console.log('Server escuchando en el puerto http://localhost:' + port + '/api')
 })
